37f8ad514e878d65d95250ce944d332249aeefeb
MANVE\User <lordlex63@yandex.ru>
2020-08-27 15:41:41 +0300

    feat: add battle log function

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ const PIKACHU = {
     ability: {
         thundershock: {
             name: 'Удар грома',
-            power: 7
+            power: 7,
         },
         quickAttack: {
             name: 'Быстрая атака',
@@ -76,11 +76,70 @@ function attack(action, targetPokemon) {
         if (key == action) {
             const attackPower = random(this.ability[key].power)
 
-            console.log(`Покемон ${this.name} использует способность ${this.ability[key].name} на покемона ${targetPokemon.name} силой ${attackPower}`);
             changeHP(attackPower, targetPokemon);
+            console.log(generateLog(this, targetPokemon, attackPower, key));
             return
         }
     }
+}
+
+function generateLog(attackerPokemon, targetPokemon, attackPower, abilityId) {
+    let logsArr;
+
+    const missLog = `О нет! ${attackerPokemon.name} промахнулся.`
+
+    switch (abilityId) {
+        case 'thundershock':
+            logsArr = [
+                `${attackerPokemon.name} заряжает свои щечки и атакует ${targetPokemon.name} на ${attackPower}.`,
+                `Пока ${targetPokemon.name} отвлекся, ${attackerPokemon.name} накопил заряд и нанес электрический удар силой ${attackPower}.`
+            ];
+            break;
+        case 'quickAttack':
+            logsArr = [
+                `${attackerPokemon.name} молниеносно атакует ${targetPokemon.name} на ${attackPower}.`,
+                `${attackerPokemon.name} очень быстро бьет своими лапками ${targetPokemon.name} нанося ему ${attackPower} урона.`,
+            ];
+            break;
+        case 'slam':
+            logsArr = [
+                `${attackerPokemon.name} неожиданно толкает ${targetPokemon.name} плечом, чем наносит ему ${attackPower} урона.`,
+                `${attackerPokemon.name} наносит ${attackPower} урона ${targetPokemon.name} ударив его хвостом.`
+            ];
+            break;
+        case 'thunderbolt':
+            logsArr = [
+                `${attackerPokemon.name} наносит невероятно мощную электрическую атаку ${targetPokemon.name}. ${targetPokemon.name} получил ${attackPower} урона.`,
+                `${attackerPokemon.name} ударяет молнией ${targetPokemon.name} и наносит ему ${attackPower} урона.`
+            ];
+            break;
+        case 'scratch':
+            logsArr = [
+                `${attackerPokemon.name} царапет своими острыми когтями ${targetPokemon.name}. ${targetPokemon.name} получил ${attackPower} урона.`,
+                `Острые когти ${attackerPokemon.name} царапают ${targetPokemon.name} на ${attackPower} урона.`,
+            ];
+            break;
+        case 'ember':
+            logsArr = [
+                `${attackerPokemon.name} поражает маленькими огоньками ${targetPokemon.name}. ${targetPokemon.name} получил ${attackPower} урона.`,
+                `Огоньки ${attackerPokemon.name} поражают ${targetPokemon.name} на ${attackPower} урона.`,
+            ];
+            break;
+        case 'fireFang':
+            logsArr = [
+                `${attackerPokemon.name} кусает раскаленными зубами ${targetPokemon.name}. ${targetPokemon.name} получил ${attackPower} урона.`,
+                `Расскаленные до бела зубы ${attackerPokemon.name} впиваются бедного ${targetPokemon.name} и наносят ему ${attackPower} урона.`,
+            ];
+            break;
+        case 'flameBurst':
+            logsArr = [
+                `${attackerPokemon.name} поражает ${targetPokemon.name} огненным взрывом. ${targetPokemon.name} получил ${attackPower} урона.`,
+                `Огненный взрыв ${attackerPokemon.name} поражает ${targetPokemon.name} и наносят ему ${attackPower} урона.`,
+            ];
+            break;
+    }
+
+    return attackPower === 0 ? missLog : `${logsArr[((Math.ceil(Math.random() * 2)) - 1)]} ${attackerPokemon.name} [${attackerPokemon.currentHP} / ${attackerPokemon.fullHP}], ${targetPokemon.name} [${targetPokemon.currentHP} / ${targetPokemon.fullHP}]`; 
 }
 
 function renderHP() {
@@ -97,13 +156,14 @@ function renderHPProgressBar() {
 }
 
 function changeHP(attackPower, targetPokemon) {
-    if (targetPokemon.currentHP < attackPower) {
+    targetPokemon.currentHP -= attackPower;
+
+    if (targetPokemon.currentHP <= 0) {
         targetPokemon.currentHP = 0;
+
         alert(`Покемон ${targetPokemon.name} проиграл бой!`);
         youPokemon.endGame();
         enemyPokemon.endGame();
-    } else {
-        targetPokemon.currentHP -= attackPower;
     }
 
     targetPokemon.renderHP();
@@ -120,13 +180,7 @@ function endGame() {
 }
 
 function random(power) {
-    const damage = Math.round(Math.random() * power);
-
-    if (damage == 0) {
-        alert('Miss!');
-    }
-
-    return damage;
+    return Math.floor(Math.random() * power);
 }
 
 function init() {
